ec9744a151a26f0530ac23441a982d9d651f9224
Fernando <fernandodecamargo.fdc@gmail.com>
2022-03-10 15:52:42 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import Model.Gatinho;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gatinho Gatinho = new Gatinho();
+        System.out.println(Gatinho);
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
